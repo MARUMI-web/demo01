6da8cf48dca767446f6a2601012d0a8a46c0cdb6
MARUMI-web <87695749+MARUMI-web@users.noreply.github.com>
2021-07-21 06:54:26 +0900

    Add files via upload

---
+++ demo/js/test.js
@@ -0,0 +1,195 @@
+//テキストのカウントアップ+バーの設定
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+    easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+    duration: 1000,//時間指定(1000＝1秒)
+    strokeWidth: 0.2,//進捗ゲージの太さ
+    color: '#555',//進捗ゲージのカラー
+    trailWidth: 0.2,//ゲージベースの線の太さ
+    trailColor: '#bbb',//ゲージベースの線のカラー
+    text: {//テキストの形状を直接指定       
+      style: {//天地中央に配置
+        position: 'absolute',
+        left: '50%',
+        top: '50%',
+        padding: '0',
+        margin: '-30px 0 0 0',//バーより上に配置
+        transform:'translate(-50%,-50%)',
+        'font-size':'1rem',
+        color: '#fff',
+      },
+      autoStyleContainer: false //自動付与のスタイルを切る
+    },
+    step: function(state, bar) {
+      bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+    }
+  });
+  
+  //アニメーションスタート
+  bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+    $("#splash_text").fadeOut(10);//フェイドアウトでローディングテキストを削除
+    $(".loader_cover-up").addClass("coveranime");//カバーが上に上がるクラス追加
+    $(".loader_cover-down").addClass("coveranime");//カバーが下に下がるクラス追加
+    $("#splash").fadeOut();//#splashエリアをフェードアウト
+  });
+
+/* <section1> */
+// const fadein = () => {
+//     const target = document.querySelector("#js-fadein");
+//     if (target.classList.contains("scrollin")) {
+//         target.classList.remove("fadein");
+//         target.classList.add("scrollin");
+//     } else {
+//         target.classList.add("scrollin")
+//     }
+// };
+
+// document.addEventListener("scroll", fadein);
+
+function fadeAnime(){
+
+    // ふわっ
+    $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
+      var elemPos = $(this).offset().top-50;//要素より、50px上の
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll >= elemPos - windowHeight){
+      $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+      }else{
+      $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
+      }
+      });
+  }
+  
+  // 画面をスクロールをしたら動かしたい場合の記述
+    $(window).scroll(function (){
+      fadeAnime();/* アニメーション用の関数を呼ぶ*/
+    });// ここまで画面をスクロールをしたら動かしたい場合の記述
+  
+  // 画面が読み込まれたらすぐに動かしたい場合の記述
+    $(window).on('load', function(){
+      fadeAnime();/* アニメーション用の関数を呼ぶ*/
+    });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+  
+
+
+// greeting
+const fadeItems = document.querySelectorAll("li")
+fadeItems.forEach((item, index) => {
+ ScrollReveal().reveal(`.fadeout${index + 1}` ,{
+      delay: 100 * index,
+      duration: 800,
+      viewFactor: 1,
+    });
+})
+
+// about
+function delayScrollAnime() {
+  var time = 0.2;//遅延時間を増やす秒数の値
+  var value = time;
+  $('.delayScroll').each(function () {
+    var parent = this;          //親要素を取得
+    var elemPos = $(this).offset().top;//要素の位置まで来たら
+    var scroll = $(window).scrollTop();//スクロール値を取得
+    var windowHeight = $(window).height();//画面の高さを取得
+    var childs = $(this).children();  //子要素を取得
+    
+    if (scroll >= elemPos - windowHeight && !$(parent).hasClass("play")) {//指定領域内にスクロールが入ったらまた親要素にクラスplayがなければ
+      $(childs).each(function () {
+        
+        if (!$(this).hasClass("fadeUp")) {//アニメーションのクラス名が指定されているかどうかをチェック
+          
+          $(parent).addClass("play"); //親要素にクラス名playを追加
+          $(this).css("animation-delay", value + "s");//アニメーション遅延のCSS animation-delayを追加し
+          $(this).addClass("fadeUp");//アニメーションのクラス名を追加
+          value = value + time;//delay時間を増加させる
+          
+          //全ての処理を終わったらplayを外す
+          var index = $(childs).index(this);
+          if((childs.length-1) == index){
+            $(parent).removeClass("play");
+          }
+        }
+      })
+    }else {
+      $(childs).removeClass("fadeUp");//アニメーションのクラス名を削除
+      value = time;//delay初期値の数値に戻す
+    }
+  })
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function (){
+    delayScrollAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+  $(window).on('load', function(){
+    delayScrollAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+// section4
+var swiper = new Swiper(".swiper-container", {
+    slidesPerView: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+});
+
+// section6
+    const sideinItems = document.querySelectorAll("li")
+    sideinItems.forEach((item, index) => {
+     ScrollReveal().reveal(`.ml${index + 1}` ,{
+      delay: 200 * index,
+      duration: 800,
+      viewFactor: 1,
+      origin: 'left', 
+      distance: '50px',
+    });
+})
+
+function fadeleftAnime(){
+
+  // ふわっ
+  $('.fadeleftTrigger').each(function(){ //fadeUpTriggerというクラス名が
+    var elemPos = $(this).offset().top-50;//要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeLeft');// 画面内に入ったらfadeUpというクラス名を追記
+    }else{
+    $(this).removeClass('fadeLeft');// 画面外に出たらfadeUpというクラス名を外す
+    }
+    });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function (){
+    fadeleftAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+  $(window).on('load', function(){
+    fadeleftAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+// section7
+ScrollReveal().reveal('.bf-box', {
+ origin: 'bottom',
+ distance: '50px',
+ duration: 800,
+});
+
+// footer
+const bottomf = document.querySelector(".bottom-form");
+window.addEventListener('scroll', () => {
+const currentY = window.pageYOffset;
+if (currentY > 100) {
+    setTimeout(function () {
+        bottomf.style.opacity = 1;
+    },1);
+} else {
+    setTimeout(function() {
+        bottomf.style.opacity = 0;
+    },1);
+}
+});
